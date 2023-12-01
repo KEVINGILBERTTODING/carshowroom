4abce811906266decc88c604a602b07636115213
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-12-02 05:34:55 +0700

    Styling

--- resources/views/layouts/error/t_error.blade.php
+++ resources/views/layouts/error/t_error.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Not Found</title>
+    <title>Error</title>
 
 
     <link rel="shortcut icon" href="{{ asset('template/admin/assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
